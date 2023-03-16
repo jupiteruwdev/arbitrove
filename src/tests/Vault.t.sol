83da9ce5729c785351b5f74104edbb6b95012a8e
jupiteruwdev <harutoinoue8@gmail.com>
2023-03-16 15:26:49 -0400

    Feat: Add consecutive tests

--- src/tests/Vault.t.sol
+++ src/tests/Vault.t.sol
@@ -15,7 +15,11 @@ contract VaultTest is Test, VyperDeployer {
     FeeOracle public feeOracle;
 
     address public whale = vm.addr(1);
+    address public user1 = vm.addr(2);
+    address public user2 = vm.addr(3);
     uint256 public mockCoin1Balance = 1e18;
+    uint256 public mockUser1Balance = 1e18;
+    uint256 public mockUser2Balance = 10e18;
     uint256 public mockVaultBalance = 1e19;
 
     function setUp() public {
@@ -40,7 +44,7 @@ contract VaultTest is Test, VyperDeployer {
         Vault(payable(factory.vaultAddress())).init829{value: 1e18}(
             AddressRegistry(factory.addressRegistryAddress())
         );
-        FeeOracle(factory.feeOracleAddress()).init(20, 0);
+        FeeOracle(factory.feeOracleAddress()).init(70, 0);
 
         /// setting up initial data
         CoinWeight[] memory cw = new CoinWeight[](2);
@@ -49,6 +53,8 @@ contract VaultTest is Test, VyperDeployer {
         vault = Vault(payable(factory.vaultAddress()));
         jonesToken = new MockERC20("Jones Token", "JONES");
         jonesToken.mint(whale, mockCoin1Balance);
+        jonesToken.mint(user1, mockUser1Balance);
+        jonesToken.mint(user2, mockUser2Balance);
         jonesToken.mint(address(vault), mockVaultBalance);
         cw[0] = CoinWeight(address(0), 50);
         cw[1] = CoinWeight(address(jonesToken), 50);
@@ -62,7 +68,7 @@ contract VaultTest is Test, VyperDeployer {
         vm.stopPrank();
     }
 
-    function test_setup() public {
+    function testSetup() public {
         CoinWeight[] memory cw = new CoinWeight[](2);
         cw[0] = CoinWeight(address(0), 50);
         cw[1] = CoinWeight(address(jonesToken), 50);
@@ -193,5 +199,274 @@ contract VaultTest is Test, VyperDeployer {
             "!expected whale's vault balance"
         );
         vm.stopPrank();
+    }
+
+    function testMultiDepositAndWithdraw() public {
+        multipleDeposit();
+        multipleWithdraw();
+    }
+
+    function multipleDeposit() internal {
+        CoinPriceUSD[] memory x = new CoinPriceUSD[](2);
+
+        /// submit user2 mint request
+        vm.startPrank(user2);
+        jonesToken.approve(address(router), mockUser2Balance);
+        router.submitMintRequest(
+            MintRequest(
+                mockUser2Balance,
+                1000000000000,
+                jonesToken,
+                user2,
+                block.timestamp + 1 days
+            )
+        );
+        vm.stopPrank();
+
+        /// submit user1 mint request
+        vm.startPrank(user1);
+        jonesToken.approve(address(router), mockUser1Balance);
+        router.submitMintRequest(
+            MintRequest(
+                mockUser1Balance,
+                1000000000000,
+                jonesToken,
+                user1,
+                block.timestamp + 1 days
+            )
+        );
+        vm.stopPrank();
+
+        vm.startPrank(whale);
+
+        /// coin price usd for user1 deposit
+        x[0] = CoinPriceUSD(address(0), 1600e4);
+        x[1] = CoinPriceUSD(address(jonesToken), 20e4);
+
+        /// depsosit fee params for user1 deposit
+        DepositFeeParams memory depositFeeParams = DepositFeeParams({
+            cpu: x,
+            vault: vault,
+            expireTimestamp: block.timestamp + 1 days,
+            position: 1,
+            amount: mockUser1Balance
+        });
+
+        /// expected user1 vault balance after deposit
+        uint256 expectedUser1VaultBalance = getExpectedDepositAmount(
+            depositFeeParams
+        );
+
+        /// process mint request in the queue for user1
+        router.acquireLock();
+        router.processMintRequest(OracleParams(x, block.timestamp + 1 days));
+        router.releaseLock();
+
+        /// coin price usd for user2 deposit
+        x[0] = CoinPriceUSD(address(0), 1600e4);
+        x[1] = CoinPriceUSD(address(jonesToken), 200e4);
+
+        /// deposit fee params for user2 deposit
+        depositFeeParams = DepositFeeParams({
+            cpu: x,
+            vault: vault,
+            expireTimestamp: block.timestamp + 1 days,
+            position: 1,
+            amount: mockUser2Balance
+        });
+
+        /// expected user2 vault balance after deposit
+        uint256 expectedUser2VaultBalance = getExpectedDepositAmount(
+            depositFeeParams
+        );
+
+        /// process mint request in the queue for user2
+        router.acquireLock();
+        router.processMintRequest(OracleParams(x, block.timestamp + 1 days));
+        router.releaseLock();
+
+        uint256 routerVaultBalance = vault.balanceOf(address(router));
+        uint256 user1VaultBalance = vault.balanceOf(user1);
+        uint256 user2VaultBalance = vault.balanceOf(user2);
+        uint256 jonesTokenUser1Balance = jonesToken.balanceOf(user1);
+        uint256 jonesTokenUser2Balance = jonesToken.balanceOf(user2);
+
+        assertEq(
+            user1VaultBalance,
+            expectedUser1VaultBalance,
+            "!user1 vault balance after deposit"
+        );
+        assertEq(
+            user2VaultBalance,
+            expectedUser2VaultBalance,
+            "!user2 vault balance after deposit"
+        );
+        assertEq(routerVaultBalance, 0, "!router vault balance after deposit");
+        assertEq(
+            jonesTokenUser1Balance,
+            0,
+            "!user1 jonesToken balance after deposit"
+        );
+        assertEq(
+            jonesTokenUser2Balance,
+            0,
+            "!user2 jonesToken balance after deposit"
+        );
+        vm.stopPrank();
+    }
+
+    function multipleWithdraw() internal {
+        CoinPriceUSD[] memory x = new CoinPriceUSD[](2);
+        uint256 initialUser1VaultBalance = vault.balanceOf(user1);
+        uint256 initialUser2VaultBalance = vault.balanceOf(user2);
+
+        /// submit user2 burn request
+        vm.startPrank(user2);
+        vault.approve(address(router), initialUser2VaultBalance);
+        router.submitBurnRequest(
+            BurnRequest(
+                initialUser2VaultBalance,
+                mockUser2Balance / 2,
+                jonesToken,
+                user2,
+                block.timestamp + 1 days
+            )
+        );
+        vm.stopPrank();
+
+        /// submit user1 burn request
+        vm.startPrank(user1);
+        vault.approve(address(router), initialUser1VaultBalance);
+        router.submitBurnRequest(
+            BurnRequest(
+                initialUser1VaultBalance,
+                mockUser1Balance / 2,
+                jonesToken,
+                user1,
+                block.timestamp + 1 days
+            )
+        );
+        vm.stopPrank();
+
+        vm.startPrank(whale);
+
+        /// coin price usd for user1 withdraw
+        x[0] = CoinPriceUSD(address(0), 1600e4);
+        x[1] = CoinPriceUSD(address(jonesToken), 30e4);
+
+        /// depsosit fee params for user1 withdraw
+        WithdrawalFeeParams memory withdrawalFeeParams = WithdrawalFeeParams({
+            cpu: x,
+            vault: vault,
+            expireTimestamp: block.timestamp + 1 days,
+            position: 1,
+            amount: mockUser1Balance / 2
+        });
+
+        /// expected user1 vault balance after withdraw
+        uint256 expectedUser1VaultBalance = getExpectedWithdrawalAmount(
+            withdrawalFeeParams
+        );
+
+        /// process burn request in the queue for user1
+        router.acquireLock();
+        router.processBurnRequest(OracleParams(x, block.timestamp + 1 days));
+        router.releaseLock();
+
+        /// coin price usd for user2 withdraw
+        x[0] = CoinPriceUSD(address(0), 1600e4);
+        x[1] = CoinPriceUSD(address(jonesToken), 200e4);
+
+        /// withdraw fee params for user2 withdraw
+        withdrawalFeeParams = WithdrawalFeeParams({
+            cpu: x,
+            vault: vault,
+            expireTimestamp: block.timestamp + 1 days,
+            position: 1,
+            amount: mockUser2Balance / 2
+        });
+
+        /// expected user2 vault balance after withdraw
+        uint256 expectedUser2VaultBalance = getExpectedWithdrawalAmount(
+            withdrawalFeeParams
+        );
+
+        /// process burn request in the queue for user2
+        router.acquireLock();
+        router.processBurnRequest(OracleParams(x, block.timestamp + 1 days));
+        router.releaseLock();
+
+        uint256 routerVaultBalance = vault.balanceOf(address(router));
+        uint256 user1VaultBalance = vault.balanceOf(user1);
+        uint256 user2VaultBalance = vault.balanceOf(user2);
+        uint256 jonesTokenUser1Balance = jonesToken.balanceOf(user1);
+        uint256 jonesTokenUser2Balance = jonesToken.balanceOf(user2);
+
+        assertEq(
+            user1VaultBalance,
+            initialUser1VaultBalance - expectedUser1VaultBalance,
+            "!user1 vault balance after withdraw"
+        );
+        assertEq(
+            user2VaultBalance,
+            initialUser2VaultBalance - expectedUser2VaultBalance,
+            "!user2 vault balance after withdraw"
+        );
+        assertEq(routerVaultBalance, 0, "!router vault balance after withdraw");
+        assertEq(
+            jonesTokenUser1Balance,
+            mockUser1Balance / 2,
+            "!user1 jonesToken balance after withdraw"
+        );
+        assertEq(
+            jonesTokenUser2Balance,
+            mockUser2Balance / 2,
+            "!user2 jonesToken balance after withdraw"
+        );
+        vm.stopPrank();
+    }
+
+    function getExpectedDepositAmount(
+        DepositFeeParams memory depositFeeParams
+    ) internal returns (uint256) {
+        address coin = depositFeeParams.cpu[depositFeeParams.position].coin;
+        uint256 __decimals = coin == address(0)
+            ? 18
+            : IERC20Metadata(coin).decimals();
+        (int256 fee, , uint256 tvlUSD10000X) = ar.feeOracle().getDepositFee(
+            depositFeeParams
+        );
+        uint256 expectedDepositValue = (depositFeeParams.amount *
+            depositFeeParams.cpu[depositFeeParams.position].price) /
+            10 ** __decimals;
+        uint256 expectedPoolRatio = (expectedDepositValue * 1e18) /
+            tvlUSD10000X;
+
+        uint256 expectedVaultBalance = (((expectedPoolRatio *
+            vault.totalSupply()) / 1e18) * uint256(100 - fee)) / 100;
+        return expectedVaultBalance;
+    }
+
+    function getExpectedWithdrawalAmount(
+        WithdrawalFeeParams memory withdrawalFeeParams
+    ) internal returns (uint256) {
+        address coin = withdrawalFeeParams
+            .cpu[withdrawalFeeParams.position]
+            .coin;
+        uint256 __decimals = coin == address(0)
+            ? 18
+            : IERC20Metadata(coin).decimals();
+        (int256 fee, , uint256 tvlUSD10000X) = ar.feeOracle().getWithdrawalFee(
+            withdrawalFeeParams
+        );
+        uint256 expectedWithdrawalValue = (withdrawalFeeParams.amount *
+            withdrawalFeeParams.cpu[withdrawalFeeParams.position].price) /
+            10 ** __decimals;
+        uint256 expectedPoolRatio = (expectedWithdrawalValue * 1e18) /
+            tvlUSD10000X;
+
+        uint256 expectedVaultBalance = (((expectedPoolRatio *
+            vault.totalSupply()) / 1e18) * uint256(100 - fee)) / 100;
+        return expectedVaultBalance;
     }
 }
